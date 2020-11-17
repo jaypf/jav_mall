222c35c3de9f2bd756b2ced1b2096022b8e9e97b
jaypf <335055823@qq.com>
2020-11-17 17:01:54 +0800

    扣减库存Lua脚本

---
+++ mall-core/src/main/java/org/jay/mall/constant/CommonConstant.java
@@ -0,0 +1,17 @@
+package org.jay.mall.constant;
+
+/**
+ * @ClassName CommonConstant
+ * @Description TODO
+ * @Author Jay.Jia
+ * @Date 2020/11/12 17:48
+ * @Version 1.0
+ */
+public class CommonConstant {
+
+    /**
+     * 库存没有初始化，库存key在redis里面不存在
+     */
+    public static final long UNINITIALIZED_STOCK = -3L;
+
+}
